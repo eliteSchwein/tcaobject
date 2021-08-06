6999ad603ca81e5292b96659383eb856a5b1e38c
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-06 17:08:17 +0200

    add icon

--- Configuration/TCA/tx_tcaobject_domain_model_example.php
+++ Configuration/TCA/tx_tcaobject_domain_model_example.php
@@ -5,6 +5,7 @@ use ThomasLudwig\Tcaobject\TCA\DefaultTCA;
 $tca = new DefaultTCA();
 $tca->setTitle('LLL:EXT:tcaobject/Resources/Private/Language/locallang_db.xlf:tx_tcaobject_domain_model_example');
 $tca->setLabel('string_example');
+$tca->setIconFile('EXT:tcaobject/Resources/Public/Icons/tx_tcaobject_domain_model_example.gif');
 
 $stringInput = new \ThomasLudwig\Tcaobject\TCA\Inputs\TCAInputInput();
 $stringInput->setDatasetName('string_example');
